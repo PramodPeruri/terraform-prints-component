d413712619533da26402db5d92f0b4321f8935a5
pramod <peruripramoddevops@gmail.com>
2026-08-18 14:15:40 +0530

    infra

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+    type = string
+    default = "prints"
+
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+
+}
